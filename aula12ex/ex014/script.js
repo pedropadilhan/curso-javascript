bce4c807278cb90529eb44141b8098360f93699b
Pedro Padilha Neto <pedropadilhan@gmail.com>
2022-07-04 16:48:58 -0300

    Exercicio horas do dia

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,20 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = 22 //data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora < 12){
+        //BOM DIA!
+        img.src = 'img/manha.jpg'
+        document.body.style.background = '#f1cfbb'
+    } else if(hora >= 12 && hora <= 18){
+        //BOA TARDE!
+        img.src = 'img/tarde.jpg'
+        document.body.style.background = '#fcad05'
+    } else {
+        //BOA NOITE!
+        img.src = 'img/noite.jpg'
+        document.body.style.background = '#011f37'
+    }
+}
